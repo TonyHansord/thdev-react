a6b7074081b34576bd7422b2711cbe81c33660f8
Anthony Hansord <tonyhansorddev@gmail.com>
2024-01-26 14:49:21 +1100

    fixed response issue on email server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const express = require('express');
-const transporter = require('./config');
-const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
-const app = express();
+const path = require('path')
+const express = require('express')
+const transporter = require('./config')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express()
 
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(cors());
-app.use(express.json());
-app.use(express.static(buildPath));
+const buildPath = path.join(__dirname, '..', 'build')
+app.use(cors())
+app.use(express.json())
+app.use(express.static(buildPath))
 
-function sendEmail({ email, name, message }) {
+function sendEmail ({ email, name, message }) {
   return new Promise((resolve, reject) => {
     const mailOptions = {
       from: email, // sender address
@@ -25,28 +25,34 @@ function sendEmail({ email, name, message }) {
       <li>Email: ${email}</li>
       <li>Message: ${message}</li>
     </ul>
-    `,
-    };
+    `
+    }
 
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
-        console.log(error);
-        return reject(new Error(`An error has occurred`));
+        console.log(error)
+        return reject(new Error(`An error has occurred`))
+      } else {
+        return resolve({
+          status: 'ok',
+          message: `Email sent successfully`
+        })
       }
-    });
-  });
+    })
+  })
 }
 
 app.post('/send', (req, res) => {
-  console.log(req);
   sendEmail(req.body)
-    .then((response) => {
-      console.log(response);
-      res.send(response.message);
+    .then(response => {
+      res.send(response)
+      console.log(response)
+      console.log(req.body)
+      console.log(response.message)
     })
-    .catch((error) => res.status(500).send(error.message));
-});
+    .catch(error => res.status(500).send(error.message))
+})
 
 app.listen(3000, () => {
-  console.log('server start on port 3000');
-});
+  console.log('server start on port 3000')
+})
--- src/Components/ContactForm.tsx
+++ src/Components/ContactForm.tsx
@@ -1,121 +1,110 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-function ContactForm() {
+function ContactForm () {
   const [state, setState] = useState({
     name: '',
     email: '',
-    message: '',
-  });
-  const [result, setResult] = useState({ success: false, message: '' });
+    message: ''
+  })
 
   const sendEmail = async (event: React.SyntheticEvent) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    console.log('submitted');
+    console.log('submitted')
     await fetch('/send', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         name: state.name,
         email: state.email,
-        message: state.message,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-
-        console.log(data)
-
-        setResult({
-          success: true,
-          message: data,
-        });
-        setState({
-          name: '',
-          email: '',
-          message: '',
-        });
+        message: state.message
       })
-  };
+    }).then(res => {
+      console.log(res)
+      setState({
+        name: '',
+        email: '',
+        message: ''
+      })
+    })
+  }
 
   const onInputChange = (event: any) => {
-    const { name, value } = event.currentTarget;
+    const { name, value } = event.currentTarget
     setState({
       ...state,
-      [name]: value,
-    });
-  };
+      [name]: value
+    })
+  }
 
   return (
-    <div className="container mx-auto px-4 border-2 border-black rounded">
-      <div className="contact-heading-container">
-        <h2 className="section-heading">Want to get in touch?</h2>
+    <div className='container mx-auto px-4 border-2 border-black rounded'>
+      <div className='contact-heading-container'>
+        <h2 className='section-heading'>Want to get in touch?</h2>
       </div>
-      <div className="contact-form-container">
-        <p>{result.message}</p>
-
-        <form className="contact-form" id="contactForm" onSubmit={sendEmail}>
-          <div className="main-form-container">
-            <div className="left-container">
-              <div className="form-group">
-                <label className="form-label" htmlFor="name">
+      <div className='contact-form-container'>
+        <form className='contact-form' id='contactForm' onSubmit={sendEmail}>
+          <div className='main-form-container'>
+            <div className='left-container'>
+              <div className='form-group'>
+                <label className='form-label' htmlFor='name'>
                   Name
                 </label>
                 <input
-                  type="text"
-                  name="name"
-                  className="form-control"
+                  type='text'
+                  name='name'
+                  className='form-control'
                   value={state.name}
-                  id="name"
-                  placeholder="Enter your name"
+                  id='name'
+                  placeholder='Enter your name'
                   onChange={onInputChange}
                   required
                 />
               </div>
-              <div className="form-group">
-                <label className="form-label" htmlFor="email">
+              <div className='form-group'>
+                <label className='form-label' htmlFor='email'>
                   Email
                 </label>
                 <input
-                  type="email"
-                  name="email"
+                  type='email'
+                  name='email'
                   value={state.email}
-                  className="form-control"
+                  className='form-control'
                   onChange={onInputChange}
-                  id="email"
-                  placeholder="Enter your email"
+                  id='email'
+                  placeholder='Enter your email'
                   required
                 />
               </div>
             </div>
-            <div className="right-container">
-              <div className="form-group">
-                <label className="form-label" htmlFor="message">
+            <div className='right-container'>
+              <div className='form-group'>
+                <label className='form-label' htmlFor='message'>
                   Message
                 </label>
                 <textarea
-                  className="form-control"
-                  id="message"
-                  name="message"
+                  className='form-control'
+                  id='message'
+                  name='message'
                   value={state.message}
                   onChange={onInputChange}
-                  placeholder="Enter your message"
+                  placeholder='Enter your message'
                   required
                 ></textarea>
               </div>
             </div>
           </div>
-          <div className="bottom-container">
-            <button className="contact-submit-btn" type="submit">
+          <div className='bottom-container'>
+            <button className='contact-submit-btn' type='submit'>
               Submit
             </button>
           </div>
         </form>
       </div>
     </div>
-  );
+  )
 }
 
-export default ContactForm;
+export default ContactForm
