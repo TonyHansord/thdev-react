c866231c9867ae35a90538906f8bc115dd904bd3
Anthony Hansord <tonyhansorddev@gmail.com>
2024-05-25 17:09:34 +1000

    Updated about section

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -7,8 +7,8 @@ export function About () {
     <div id='about' className='about-container'>
       <img className='tony-img' src='assets/tony-head-bw.jpg' alt='Tony' />
       <div className='heading-container'>
-        <h1 className='name'>Hello! I am Tony.</h1>
-        <h2 className='sub-heading'>I am a Full Stack Developer</h2>
+        <h1 className='name'>Hello! I'm Tony.</h1>
+        <h2 className='sub-heading'>I'm a Full Stack Developer</h2>
         <p className='about-me'>
           Based in Melbourne, I have a strong passion for solving problems and
           creating beautiful, functional websites and apps. I am a full stack
